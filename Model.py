3c69570c9917e00e09e293a8d0f614839fd81696
nandika-k <nkarnik5@outlook.com>
2025-12-06 00:12:59 -0500

    imports and read_csvs

---
+++ Model.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+race1 = pd.read_csv("race_data\Abu_Dhabi_2023_Driver_Data.csv")
+race2 = pd.read_csv("race_data\Abu_Dhabi_2023_Driver_Data.csv")
+
